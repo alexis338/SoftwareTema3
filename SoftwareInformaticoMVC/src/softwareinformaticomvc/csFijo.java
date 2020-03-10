077132a07a71fd46e5cb83846d19c2d91d06ea90
JoseEduin <JoseEduin@EDUIN>
2020-03-10 01:36:00 -0500

    Se agrego Metodos para punto fijo y csFijo

---
+++ SoftwareInformaticoMVC/src/softwareinformaticomvc/csFijo.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareinformaticomvc;
+
+/**
+ *
+ * @author EduinNahuat
+ */
+public class csFijo {
+    private int iteracion;
+    private double xi,gx,error;
+
+    public int getIteracion() {
+        return iteracion;
+    }
+
+    public void setIteracion(int iteracion) {
+        this.iteracion = iteracion;
+    }
+
+    public double getXi() {
+        return xi;
+    }
+
+    public void setXi(double xi) {
+        this.xi = xi;
+    }
+
+    public double getGx() {
+        return gx;
+    }
+
+    public void setGx(double gx) {
+        this.gx = gx;
+    }
+
+    public double getError() {
+        return error;
+    }
+
+    public void setError(double error) {
+        this.error = error;
+    }
+   
+}
