b4d581096fccc32055733d8041e99c8c60717bcc
JoseEduin <JoseEduin@EDUIN>
2020-04-03 18:51:33 -0500

    Se agrego el metodo Gauss Seidel

--- SoftwareUnidad3/src/CtrlMetodos.java
+++ SoftwareUnidad3/src/CtrlMetodos.java
@@ -83,6 +83,10 @@ public class CtrlMetodos {
         
         return lista;   
     }
+    private double funcion(double x){
+     
+        return Math.pow(2.7182818284590452353602874713527,-x);
+    }
      
      
      //Funciones de Newton
@@ -93,5 +97,41 @@ public class CtrlMetodos {
     public double DerivadaFuncion(double x) {
         return -Math.pow(2.7182818284590452353602874713527, -x) - 1;
     }
+    
+    public tblGauss Gauss(int [][]A, int [] b, int iteraciones)
+    {
+    ArrayList<CsJacobi> lista = null;
+    tblGauss tabla; 
+    int cont = 0;
+    //despejes 
+    //int x1,x2,x3;
+    double x1Anterior = 0, x2Anterior = 0, x3Anterior=0;
+    //x1 = (- A[0][1] - 2*(A[0][2]) + 9)/b[0];
+    //x2 = (- 2*(A[1][0]) - (A[1][2]) - 5)/b[1];
+    
+    lista = new ArrayList<CsJacobi> ();    
+    
+    
+            
+            while(cont != iteraciones){
+                CsJacobi fila = new CsJacobi();
+                fila.setIteraciones(cont + 1);
+                fila.setX1(((- A[0][1]*x2Anterior) - (A[0][2] * x3Anterior) + b[0])/A[0][0]);
+                fila.setX2(((-A[1][0]*x1Anterior) - (A[1][2]*x3Anterior) + b[1])/A[1][1]);
+                fila.setX3(((-A[2][0]*x1Anterior)-(A[2][1] * x2Anterior) + b[2])/A[2][2]);
+                
+                x1Anterior = fila.getX1();
+                x2Anterior = fila.getX2();
+                x3Anterior = fila.getX3();
+                fila.toString();
+                lista.add(fila);
+                cont++;
+            }          
+            tabla = new tblGauss(lista);
+            return tabla;
+
+        
+    }
+    
     
 }
---
+++ SoftwareUnidad3/src/tblGauss.java
@@ -0,0 +1,53 @@
+
+
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+
+ */
+public class tblGauss extends AbstractTableModel {
+
+   private String[] fila = {"itera","x1","x2","x3"};
+    
+    private ArrayList<CsJacobi> lista =null;
+
+    public tblGauss(ArrayList<CsJacobi> resultados) {
+        this.lista = resultados;
+    }
+
+    @Override
+    public int getRowCount() {
+        return this.fila.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return this.lista.size();
+    }
+
+    @Override
+    public Object getValueAt(int fila, int i) {
+        CsJacobi fli = this.lista.get(i);
+        
+        switch(fila){
+            case 0:
+                return fli.getIteraciones();
+            case 1:
+                return fli.getX1(); 
+            case 2: 
+                return fli.getX2();
+            case 3:
+                return fli.getX3();
+                
+        }
+        return null;
+    }
+    @Override
+    public boolean isCellEditable(int i, int i1) {
+        return true;
+    }
+    
+}
